1ae71aa8379a86ef5c7c16ddf5bf204cc2f2e206
Bas Pennings <baspster@gmail.com>
2026-02-15 15:46:09 +0100

    Oscillator test update.

--- src/Subtraktor.Tests/OscTests.fs
+++ src/Subtraktor.Tests/OscTests.fs
@@ -3,12 +3,9 @@
 open Xunit
 open Subtraktor.Osc
 
-let isApprox x eps =
-    abs (x - eps) < eps
-
 let approxEqual x y =
     let closeEnough = 1e-6
-    abs (x - y) < closeEnough
+    abs (x - y) < closeEnough   
 
 let approxZero x = approxEqual x 0.0
 
