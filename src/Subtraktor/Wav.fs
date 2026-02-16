9e373f1ff7ed7f932558c6470d37109435c1abd7
Bas Pennings <baspster@gmail.com>
2026-02-16 05:34:09 +0100

    Basic Wav.fs module skeleton.

---
+++ src/Subtraktor/Wav.fs
@@ -0,0 +1,9 @@
+﻿module Subtraktor.Wav
+
+open FSharp.Data.UnitSystems.SI.UnitSymbols
+
+let encodePcm16 (samples: float[]) : byte[] =
+    failwith "Not implemented"
+    
+let writeWav (path: string) (rate: float<Hz>) (samples: float[]) : unit =
+    failwith "Not implemented"
