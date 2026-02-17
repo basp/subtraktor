4e507808266fc17927373980b22c24230e3a6365
Bas Pennings <baspster@gmail.com>
2026-02-17 14:54:40 +0100

    Little bit of cleanup.

--- src/Subtraktor/Viz.fs
+++ src/Subtraktor/Viz.fs
@@ -1,11 +1,9 @@
 ﻿module Subtraktor.Viz
 
-open FSharp.Data.UnitSystems.SI.UnitSymbols
-
 open Subtraktor.Units
 open Subtraktor.Signal
 
-let sample (sr: float<Hz>) (duration: Time) (signal: Signal) =
+let sample (sr: SampleRate) (duration: Time) (signal: Signal) =
     let dt = 1.0 / sr
     let samples = int (duration / dt)
     Array.init samples (fun i ->
